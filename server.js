1df58b88ec8453a3f7816f4ff86e7180a4f6cc6e
htcworld <61133997+htcworld@users.noreply.github.com>
2020-03-01 03:16:00 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+// initialize dependencies
+var http = require("http");
+var spawn = require('child_process').spawn;
+// start server
+	
+	var ffmpeg = require('child_process').spawn("ffmpeg-source/ffmpeg", ["-re", "-i", "assets/input.mp4", "-c:v", "libx264", "-preset", "superfast", "-tune", "zerolatency", "-c:a", "aac", "-ar", "44100", "-f", "flv", "rtmp://hhh-rtmp.herokuapp.com:1935/live/test_stream.flv" ]);
+	
+	ffmpeg.on('error', function (err) {
+		console.log(err);
+	});
+
+	ffmpeg.on('close', function (code) {
+		console.log('ffmpeg exited with code ' + code);
+	});
+
+	ffmpeg.stderr.on('data', function (data) {
+		console.log('stderr: ' + data);
+	});
+
+	ffmpeg.stdout.on('data', function (data) {
+		
+		var frame = new Buffer(data).toString('base64');
+		socket.emit('render',frame);
+	});
+
+};
+
